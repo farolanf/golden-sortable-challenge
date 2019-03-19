121405e934f1bc4e65cb7387291478a5cd21a8b7
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-19 18:28:51 +0800

    Updates

---
+++ precache-manifest.51fd274c298682f26b9e09768d91cb6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4fffe15e2350dd118e6",
+    "url": "/static/js/main.875afcce.chunk.js"
+  },
+  {
+    "revision": "410fd1743965ee12e445",
+    "url": "/static/js/2.cf58ca57.chunk.js"
+  },
+  {
+    "revision": "d4fffe15e2350dd118e6",
+    "url": "/static/css/main.06570a8c.chunk.css"
+  },
+  {
+    "revision": "bc07b830822cf4db3c76702ac2b369ab",
+    "url": "/index.html"
+  }
+];
